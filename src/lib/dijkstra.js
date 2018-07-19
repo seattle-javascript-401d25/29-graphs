f6d9b74de3be4fe420f18097725e247cb50504a2
Devin Cunningham <dtyler.cunningham@gmail.com>
2018-07-19 14:18:19 -0700

    passing tests for BFS

---
+++ src/lib/dijkstra.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const PriorityQueue = require('js-priority-queue');
+
+module.exports = (graph, startNode, goalNode) => {
+  const visitedNodes = new Set();
+  const parentMap = new Map();
+  const shortestPathSoFar = new Map();
+
+  const priorityQueue = new PriorityQueue({
+    comparator: (a, b) => a.priority - b.priority,
+  });
+
+  priorityQueue.queue({
+    node: startNode,
+    priority: 0,
+  });
+  shortestPathSoFar.set(startNode, 0);
+
+  while (priorityQueue.length > 0) {
+    const currentNode = priorityQueue.dequeue().node;
+
+    if (visitedNodes.has(currentNode)) { continue; } /*eslint-disable-line*/
+
+    visitedNodes.add(currentNode);
+
+    if (currentNode === goalNode) { return parentMap; }
+
+    const neighbors = graph.getNeighbors(currentNode);
+
+    /*eslint-disable*/
+    for (const neighbor of neighbors) {
+      const neighborWeight = neighbor.weight;
+      const neighborNode = neighbor.node;
+
+      if (visitedNodes.has(neighborNode)) { continue; }
+      const newPathWeight = shortestPathSoFar.get(currentNode) + neighborWeight;
+
+      if (!shortestPathSoFar.has(neighborNode) || newPathWeight < shortestPathSoFar.get(neighborNode)) {
+        shortestPathSoFar.set(neighborNode, newPathWeight);
+        parentMap.set(neighborNode, currentNode);
+
+        priorityQueue.queue({
+          node: neighborNode,
+          priority: shortestPathSoFar.get(neighborNode),
+        });
+      }
+    }
+  }
+  return null;
+};
